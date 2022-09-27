c71a4016aec887dc0667c5dd4317ef16a21bfbbb
Marina Ivanova <ubcg62bs@Marinas-MacBook-Pro.local>
2022-09-27 10:54:11 +0100

    Exercise 6 - loops refactored

--- odd-even.js
+++ odd-even.js
@@ -1,7 +1,12 @@
-for (let i = 1; i <=20; i++) {
-  if (i % 2 === 0) {
-    console.log(`${i} is even`);
+const getParity = (number) => {
+  if (number % 2 === 0) {
+    return `even`;
   } else {
-    console.log(`${i} is odd`)
+    return `odd`;
   }
+}
+
+
+for (let i = 1 ; i <= 20 ; i ++ ) {
+  console.log(`${i} is ${getParity(i)}`);
 }
